96627ac2d4e4fad764d76ee67b7ec84ea2f62062
codebear-py <72389987+codebear-py@users.noreply.github.com>
2020-10-05 17:19:39 +0530

    Add files via upload

---
+++ all_tutorial_inone.py
@@ -0,0 +1,21 @@
+#strings
+name="compaqc"
+#string indexing
+print(name[-1])
+# string slicing
+print(name[-1:0:-1])
+# take user input
+user_name,age=input("enter your name and age: ").split(",")
+print(user_name)
+print(age)
+# len function
+print(len(name))
+# lower,upper,title methods
+print(name.title())
+# upper method
+print(name.upper())
+# find,replace,center method
+print(name.find("c",1))
+# center method
+print(name.center(11,"*"))
+# strings are immutable
